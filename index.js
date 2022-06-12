374ff1bbd6ccefe86a8c896ba8bca332f591818f
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-06-12 18:24:45 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(SearchArray, searchData){
+    let filteredArray = SearchArray.filter(data => data.toLowerCase() == searchData.toLowerCase());
+    return filteredArray;
+}
+function fuzzyMatch(SearchArray, searchData){
+    let filteredArray = SearchArray.filter(data => {
+        return (data.startsWith(searchData));
+    });
+        
+    return filteredArray;
+}
+ function matchName(SearchArray, searchData){
+    let filteredArray = SearchArray.filter(data => {
+        return (data.name === searchData);
+    });
+    return filteredArray;
+ }
+
+ 
